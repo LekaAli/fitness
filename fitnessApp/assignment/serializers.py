ef24040facd906cf322bd059da70500ea12f91e7
Leka <leka@101collective.com>
2018-04-29 13:41:02 +0200

    deleted app serviceprovider

--- fitnessApp/assignment/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Assignment
-from serviceprovider.models import Serviceprovider
-
-class ServiceProviderSerializer(serializers.ModelSerializer):
-    assignment = serializers.StringRelatedField(many=True)
-    class Meta:
-        model = Serviceprovider
-        fields = ('firstname','lastname','category','assignment')
--- fitnessApp/patient/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class PatientConfig(AppConfig):
-    name = 'patient'
